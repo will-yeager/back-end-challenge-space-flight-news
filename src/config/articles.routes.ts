21ee374ff3eb7de60dc48913676713c5cb87bfae
William <willyeager01@gmail.com>
2022-04-19 01:45:38 -0300

    feat(routes): articles routes of get by id, post and delete by id

--- src/config/articles.routes.ts
+++ src/config/articles.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import { CreateArticleController } from '../controllers/CreateArticleController'
+import { DeleteArticleController } from '../controllers/DeleteArticleController'
+import { GetArticleByIdController } from '../controllers/GetArticleByIdController'
 import { ListArticlesController } from '../controllers/ListArticlesController'
+
 const articlesRoutes = Router()
 
 const listArticlesController = new ListArticlesController()
+const getArticleByIdController = new GetArticleByIdController()
+const createrticleController = new CreateArticleController()
+const deleteArticleController = new DeleteArticleController()
 
 articlesRoutes.get('/', listArticlesController.handle)
+articlesRoutes.get('/:id', getArticleByIdController.handle)
+articlesRoutes.post('/', createrticleController.handle)
+articlesRoutes.delete('/:id', deleteArticleController.handle)
 
 export { articlesRoutes }
