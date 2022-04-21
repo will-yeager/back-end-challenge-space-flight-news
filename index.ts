ba79825e28e1b5b5473eed13f8f8be066867e88f
William <willyeager01@gmail.com>
2022-04-20 22:54:03 -0300

    fix: fix port for heroku build

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import { router } from './src/config';
 import { dbConnection } from './src/config/database';
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 
 dbConnection()
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Servidor rodando na porta 3000');
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
   })
   .catch((error) => {
